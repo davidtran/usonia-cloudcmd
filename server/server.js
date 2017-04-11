cd04274b9c267ccc08dcbc3442e01ae79601a345
Thang Nguyen Minh <quaywin@gmail.com>
2017-04-11 10:59:22 +0700

    update server

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const logger = tryRequire('morgan');
 
 const prefix = squad(prefixer, apart(config, 'prefix'));
 const os = require('os');
-const pty = require('node-pty');
+// const pty = require('node-pty');
 
 module.exports = (options) => {
     const port = process.env.VCAP_APP_PORT || /* cloudfoundry */
@@ -67,61 +67,61 @@ module.exports = (options) => {
         })
     }));
 
-    app.post('/terminals', function (req, res) {
-        var cols = parseInt(req.query.cols),
-            rows = parseInt(req.query.rows),
-            term = pty.spawn(process.platform === 'win32' ? 'cmd.exe' : 'bash', [], {
-                name: 'xterm-color',
-                cols: cols || 80,
-                rows: rows || 24,
-                cwd: process.env.PWD,
-                env: process.env
-            });
+    // app.post('/terminals', function (req, res) {
+    //     var cols = parseInt(req.query.cols),
+    //         rows = parseInt(req.query.rows),
+    //         term = pty.spawn(process.platform === 'win32' ? 'cmd.exe' : 'bash', [], {
+    //             name: 'xterm-color',
+    //             cols: cols || 80,
+    //             rows: rows || 24,
+    //             cwd: process.env.PWD,
+    //             env: process.env
+    //         });
 
-        console.log('Created terminal with PID: ' + term.pid);
-        terminals[term.pid] = term;
-        logs[term.pid] = '';
-        term.on('data', function(data) {
-            logs[term.pid] += data;
-        });
-        res.send(term.pid.toString());
-        res.end();
-        });
+    //     console.log('Created terminal with PID: ' + term.pid);
+    //     terminals[term.pid] = term;
+    //     logs[term.pid] = '';
+    //     term.on('data', function(data) {
+    //         logs[term.pid] += data;
+    //     });
+    //     res.send(term.pid.toString());
+    //     res.end();
+    //     });
 
-        app.post('/terminals/:pid/size', function (req, res) {
-        var pid = parseInt(req.params.pid),
-            cols = parseInt(req.query.cols),
-            rows = parseInt(req.query.rows),
-            term = terminals[pid];
+    //     app.post('/terminals/:pid/size', function (req, res) {
+    //     var pid = parseInt(req.params.pid),
+    //         cols = parseInt(req.query.cols),
+    //         rows = parseInt(req.query.rows),
+    //         term = terminals[pid];
 
-        term.resize(cols, rows);
-        console.log('Resized terminal ' + pid + ' to ' + cols + ' cols and ' + rows + ' rows.');
-        res.end();
-        });
+    //     term.resize(cols, rows);
+    //     console.log('Resized terminal ' + pid + ' to ' + cols + ' cols and ' + rows + ' rows.');
+    //     res.end();
+    //     });
 
-        app.ws('/terminals/:pid', function (ws, req) {
-        var term = terminals[parseInt(req.params.pid)];
-        console.log('Connected to terminal ' + term.pid);
-        ws.send(logs[term.pid]);
+    //     app.ws('/terminals/:pid', function (ws, req) {
+    //     var term = terminals[parseInt(req.params.pid)];
+    //     console.log('Connected to terminal ' + term.pid);
+    //     ws.send(logs[term.pid]);
 
-        term.on('data', function(data) {
-            try {
-            ws.send(data);
-            } catch (ex) {
-            // The WebSocket is not open, ignore
-            }
-        });
-        ws.on('message', function(msg) {
-            term.write(msg);
-        });
-        ws.on('close', function () {
-            term.kill();
-            console.log('Closed terminal ' + term.pid);
-            // Clean things up
-            delete terminals[term.pid];
-            delete logs[term.pid];
-        });
-    });
+    //     term.on('data', function(data) {
+    //         try {
+    //         ws.send(data);
+    //         } catch (ex) {
+    //         // The WebSocket is not open, ignore
+    //         }
+    //     });
+    //     ws.on('message', function(msg) {
+    //         term.write(msg);
+    //     });
+    //     ws.on('close', function () {
+    //         term.kill();
+    //         console.log('Closed terminal ' + term.pid);
+    //         // Clean things up
+    //         delete terminals[term.pid];
+    //         delete logs[term.pid];
+    //     });
+    // });
 
     if (port < 0 || port > 65535)
         exit('cloudcmd --port: %s', 'port number could be 1..65535, 0 means any available port');
